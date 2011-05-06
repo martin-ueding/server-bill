fcdd30f0745378d3137ad23de3a3fd2fdb7ca4f0
Martin Ueding <dev@martin-ueding.de>
2011-05-06 14:36:15 +0200

    worked on status

--- model.py
+++ model.py
@@ -46,8 +46,42 @@ class Package(Entity):
 
 	hoster_bills = OneToMany("HosterBill")
 
-	def __init__(self):
-		self.due_date = PackageDueDate(self)
+	@property
+	def status(self):
+		status_msgs = []
+		last_bill = self.getLastBill()
+		# fully payed
+		if last_bill is not None and not self.needs_payment:
+			status_msgs.append(gettext("payed by you"))
+
+		if last_bill is None:
+			status_msgs.append(gettext("cannot determine status"))
+
+		# needs payment but has no bill
+		if self.needs_payment:
+			status_msgs.append(gettext("hoster bill is due"))
+
+		# unpaid
+		if last_bill is not None and not last_bill.isPayed:
+			status_msgs.append(gettext("last bill not payed by you"))
+
+		# unrelayed
+		if last_bill is not None and not last_bill.isRelayed:
+			status_msgs.append(gettext("not relayed to customer"))
+
+			if last_bill.own_bill[0].is_payed:
+				status_msgs.append(gettext("payed by customer"))
+			else:
+				status_msgs.append(gettext("not payed by customer"))
+
+		elif last_bill is not None:
+			status_msgs.append(gettext("relayed to customer"))
+
+		
+
+		return ', '.join(status_msgs)
+
+	
 
 	def __repr__(self):
 		if self.customer is None:
@@ -65,8 +99,7 @@ class Package(Entity):
 
 		return gettext("Package %s of %s") % (self.aDomain, display_customer)
 
-	@property
-	def nextDueDate(self):
+	def getLastBill(self):
 		if self.hoster_bills is None:
 			logging.warning(gettext("%s has no hoster_bill field") % self.__repr__())
 			return None
@@ -75,16 +108,42 @@ class Package(Entity):
 			logging.warning(gettext("%s has no hoster_bills") % self.__repr__())
 			return None
 
+		last_bill = None
 		last_bill_date = datetime.date(day=1, month=1, year=1)
 
 		for bill in self.hoster_bills:
 			if last_bill_date < bill.date:
+				last_bill = bill
 				last_bill_date = bill.date
 
+		return last_bill
+
+	@property
+	def last_bill_date(self):
+		if self.getLastBill() is not None:
+			return self.getLastBill().date
+
+		return None
+
+
+	@property
+	def nextDueDate(self):
+		last_bill_date = self.last_bill_date
+
+		if last_bill_date is None:
+			return None
+		
 		# add the interval to the date
 		due_date = datetime.date(day=last_bill_date.day, month=(last_bill_date.month-1+self.interval_months)%12 +1, year=last_bill_date.year + (last_bill_date.month-1+self.interval_months)/12)
 
 		return due_date
+
+	@property
+	def needs_payment(self):
+		if self.nextDueDate is None:
+			return False
+
+		return self.nextDueDate < datetime.date.today()
 
 	@property
 	def aDomain(self):
@@ -98,7 +157,7 @@ class Package(Entity):
 		verbose_name = gettext("Package")
 		verbose_name_plural = gettext("Packages")
 
-		list_display = ['interval_months', 'customer', 'hoster_customer_number', 'domains', 'aDomain', 'nextDueDate']
+		list_display = ['interval_months', 'customer', 'hoster_customer_number', 'domains', 'aDomain', 'last_bill_date', 'nextDueDate', 'needs_payment', 'status']
 
 class HosterBill(Entity):
 	date = Field(Date)
